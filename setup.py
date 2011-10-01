ffd8758b90819059dd1fdbdde1f8f189a9c7c07c
Christophe de Vienne <cdevienne@gmail.com>
2011-10-01 14:15:44 +0200

    completed the setup informations

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
+import os
+
 from setuptools import setup
 
+execfile(os.path.join('wsme', 'release.py'))
+
 setup(
-    name='wsme',
+    name=name,
+    version=version,
+    description=description,
+    long_description=long_description,
+    author=author,
+    author_email=email,
+    url=url,
     packages=['wsme'],
     install_requires=[
         'simplegeneric',
         'webob',
     ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: WSGI'
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
---
+++ wsme/release.py
@@ -0,0 +1,14 @@
+name = "WSME"
+version = "0.1"
+
+description = "Web Services Made Easy"
+long_description = """
+Web Service Made Easy is a pure-wsgi and modular rewrite of TGWebServices.
+"""
+
+author = "Christophe de Vienne"
+email = "cdevienne@gmail.com"
+
+url = "http://bitbucket.org/cdevienne/wsme"
+
+license = "MIT"
