aecf045fb0a76e4afc72efaeb72840af83eb32a1
Christophe de Vienne <cdevienne@gmail.com>
2011-10-20 14:41:08 +0200

    Completed the soap client example now that wsme-soap seems to work

--- examples/demo/client.py
+++ examples/demo/client.py
@@ -5,3 +5,7 @@ url = 'http://127.0.0.1:8989/api.wsdl'
 client = Client(url, cache=None)
 
 print client
+
+print client.service.Multiply(4, 5)
+print client.service.Helloworld()
+print client.service.Getperson()
