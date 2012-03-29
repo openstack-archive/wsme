f8c53d5ed08f5b1fd42dd30803d6fe2c2dff6848
Christophe de Vienne <cdevienne@gmail.com>
2012-03-29 15:44:05 +0200

    Re-put the agogo theme (sphinxdoc was just a test

--- doc/conf.py
+++ doc/conf.py
@@ -92,7 +92,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinxdoc'
+html_theme = 'agogo'
 
 html_style = 'wsme.css'
 
