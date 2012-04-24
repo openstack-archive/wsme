633c09bbc5053679e609c006974e87014607f713
Christophe de Vienne <cdevienne@gmail.com>
2012-04-24 10:57:22 +0200

    Python 3.2 compat

--- wsme/tests/test_exc.py
+++ wsme/tests/test_exc.py
@@ -1,6 +1,7 @@
 # encoding=utf8
 
 from wsme.exc import *
+from six import u
 
 
 def test_clientside_error():
@@ -13,19 +14,19 @@ def test_invalidinput():
     e = InvalidInput('field', 'badvalue', "error message")
 
     assert e.faultstring == \
-        u"Invalid input for field/attribute field. Value: 'badvalue'. " \
-        "error message", e.faultstring
+        u("Invalid input for field/attribute field. Value: 'badvalue'. " \
+        "error message"), e.faultstring
 
 
 def test_missingargument():
     e = MissingArgument('argname', "error message")
 
     assert e.faultstring == \
-        u'Missing argument: "argname": error message', e.faultstring
+        u('Missing argument: "argname": error message'), e.faultstring
 
 
 def test_unknownargument():
     e = UnknownArgument('argname', "error message")
 
     assert e.faultstring == \
-        u'Unknown argument: "argname": error message', e.faultstring
+        u('Unknown argument: "argname": error message'), e.faultstring
