a2361f2f0306c226330bd7336a69227112d5a994
Christophe de Vienne <cdevienne@gmail.com>
2012-05-12 23:33:23 +0200

    Now cover wsme.protocols.__init__

--- wsme/tests/test_protocols.py
+++ wsme/tests/test_protocols.py
@@ -58,3 +58,14 @@ class TestProtocols(unittest.TestCase):
         r = WSRoot(['dummy'])
         assert len(r.protocols) == 1
         assert r.protocols[0].__class__ == DummyProtocol
+
+    def test_Protocol(self):
+        p = wsme.protocols.Protocol()
+        assert p.iter_calls(None) is None
+        assert p.extract_path(None) is None
+        assert p.read_arguments(None) is None
+        assert p.encode_result(None, None) is None
+        assert p.encode_sample_value(None, None) == ('none', 'N/A')
+        assert p.encode_sample_params(None) == ('none', 'N/A')
+        assert p.encode_sample_result(None, None) == ('none', 'N/A')
+
