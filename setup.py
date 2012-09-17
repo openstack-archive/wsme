e295d01c164af646095ab1189e370ac69c92242c
Christophe de Vienne <cdevienne@gmail.com>
2012-09-17 23:25:13 +0200

    Still need to specify the requires int the setup.py, as d2to1 does not seem to handle python_version dependant metadata

--- setup.py
+++ setup.py
@@ -1,2 +1,19 @@
 from setuptools import setup
-setup(setup_requires=['d2to1'], d2to1=True)
+import sys
+
+if sys.version_info[:2] <= (2, 5):
+    webob_version = ' <= 1.1.1'
+elif sys.version_info[:2] >= (3, 0):
+    webob_version = ' >= 1.2.2'
+else:
+    webob_version = ''
+
+setup(
+    setup_requires=['d2to1'],
+    install_requires=[
+        'six',
+        'simplegeneric',
+        'WebOb' + webob_version
+    ],
+    d2to1=True
+)
