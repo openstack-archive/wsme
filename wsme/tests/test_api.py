c9dd7697b340e4300f3bb0d02b0aafb48462b1bb
Julien Danjou <julien@danjou.info>
2013-09-18 16:37:31 +0200

    json: convert value to string before encoding
    If the JSON sent is not really a string, it's safer to try to convert it
    to a string before calling the encode method. This way we can accept
    integer as potentially valid strings.
    
    Change-Id: Iad0b48f597a8c062cdb05c9bbb5342df15583edf

--- wsme/tests/test_api.py
+++ wsme/tests/test_api.py
@@ -87,6 +87,30 @@ Invalid value (should be one of:"))
         self.assertIn('None', res.json_body['faultstring'])
         self.assertEqual(res.status_int, 400)
 
+    def test_validate_enum_with_wrong_type(self):
+        class Version(object):
+            number = types.Enum(str, 'v1', 'v2', None)
+
+        class MyWS(WSRoot):
+            @expose(str)
+            @validate(Version)
+            def setcplx(self, version):
+                return version.version
+
+        r = MyWS(['restjson'])
+        app = webtest.TestApp(r.wsgiapp())
+        res = app.post_json('/setcplx', params={'version': {'number': 1}},
+                            expect_errors=True,
+                            headers={'Accept': 'application/json'})
+        self.assertTrue(
+            res.json_body['faultstring'].startswith(
+                "Invalid input for field/attribute number. Value: '1'. \
+Invalid value (should be one of:"))
+        self.assertIn('v1', res.json_body['faultstring'])
+        self.assertIn('v2', res.json_body['faultstring'])
+        self.assertIn('None', res.json_body['faultstring'])
+        self.assertEqual(res.status_int, 400)
+
     def test_scan_api(self):
         class NS(object):
             @expose(int)
