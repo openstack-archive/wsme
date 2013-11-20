3e1b525a242423806c15e43d4c1d143135c7eb3d
Lucas Alvares Gomes <lucasagomes@gmail.com>
2013-11-20 16:32:58 +0000

    Fix printing object reference on StringType
    <object>.pattern is the correct pattern string from which the object
    was compiled from.
    
    Change-Id: Ie45cecfeb772e772828a468e0434d07748e34709

--- wsme/tests/test_types.py
+++ wsme/tests/test_types.py
@@ -326,6 +326,11 @@ Value: 'v3'. Value should be one of: v., v.",
         v.validate('A')
         self.assertRaises(ValueError, v.validate, '_')
 
+    def test_validate_string_type_pattern_exception_message(self):
+        regex = '[0-9]'
+        v = types.StringType(pattern=regex)
+        self.assertRaisesRegexp(ValueError, regex, v.validate, 'a')
+
     def test_validate_ipv4_address_type(self):
         v = types.IPv4AddressType()
         v.validate('127.0.0.1')
