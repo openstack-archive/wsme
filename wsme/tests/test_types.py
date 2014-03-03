047ade11bcbe1c2e58030662536155b71d47d17d
Ilya Kharin <akscram@gmail.com>
2014-03-03 12:09:36 +0400

    Fix validation of IPv{4,6}AddressType
    The patch fixes a validation of IPv4AddressType and IPv6AddressType. Due
    to the fact that the the validation method should returns a proper
    value. The IPv6AddressType uses a short format of an IPv6 textual
    representation.
    
    Change-Id: Ib00f9cc5012875055c882cf5a7bfce6c89f8f14d
    Closes-Bug: #1287152

--- wsme/tests/test_types.py
+++ wsme/tests/test_types.py
@@ -328,8 +328,8 @@ Value: 'v3'. Value should be one of: v., v.",
 
     def test_validate_ipv4_address_type(self):
         v = types.IPv4AddressType()
-        v.validate('127.0.0.1')
-        v.validate('192.168.0.1')
+        self.assertEqual(v.validate('127.0.0.1'), '127.0.0.1')
+        self.assertEqual(v.validate('192.168.0.1'), '192.168.0.1')
         self.assertRaises(ValueError, v.validate, '')
         self.assertRaises(ValueError, v.validate, 'foo')
         self.assertRaises(ValueError, v.validate,
@@ -337,8 +337,9 @@ Value: 'v3'. Value should be one of: v., v.",
 
     def test_validate_ipv6_address_type(self):
         v = types.IPv6AddressType()
-        v.validate('0:0:0:0:0:0:0:1')
-        v.validate('2001:0db8:bd05:01d2:288a:1fc0:0001:10ee')
+        self.assertEqual(v.validate('0:0:0:0:0:0:0:1'), '::1')
+        self.assertEqual(v.validate('2001:0db8:bd05:01d2:288a:1fc0:0001:10ee'),
+                         '2001:db8:bd05:1d2:288a:1fc0:1:10ee')
         self.assertRaises(ValueError, v.validate, '')
         self.assertRaises(ValueError, v.validate, 'foo')
         self.assertRaises(ValueError, v.validate, '192.168.0.1')
