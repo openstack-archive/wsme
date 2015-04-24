openstack-archive/wsme

commit 8710dabb652dae775dee31789e91608f832e62e6

Improve Accept and Content-Type handling
Originally, if WSME received an Accept or Content-Type header that
was not aligned with what it was prepared to handle it would error
out with a 500 status code. This is not good behavior for a web
service.

In the process of trying to fix this it was discovered that the
content-negotiation code within WSME (the code that, in part,
looks for a suitable protocol handler for a request) and tests of
that code are incorrect, violating expected HTTP behaviors. GET
requests are passing Content-Type headers to declare the desired
type of representation in the response. This is what Accept is for.

Unfortunately the server-side code was perfectly willing to accept
this behavior. These changes correct that.

Closes-Bug: 1419110
Change-Id: I2b5c0075611490c047b27b1b43b0505fc5534b3b

wsme/protocol.py
@@ -2,6 +2,9 @@ import weakref
 
 import pkg_resources
 
+from wsme.exc import ClientSideError
+
+
 __all__ = [
     'CallContext',
 
@@ -111,3 +114,35 @@ def getprotocol(name, **options):
             raise ValueError("Cannot find protocol '%s'" % name)
         registered_protocols[name] = protocol_class
     return protocol_class(**options)
+
+
+def media_type_accept(request, content_types):
+    """Return True if the requested media type is available.
+
+    When request.method is GET or HEAD compare with the Accept header.
+    When request.method is POST, PUT or PATCH compare with the Content-Type
+    header.
+    When request.method is DELETE media type is irrelevant, so return True.
+    """
+    if request.method in ['GET', 'HEAD']:
+        if request.accept:
+            if request.accept.best_match(content_types):
+                return True
+            error_message = ('Unacceptable Accept type: %s not in %s'
+                             % (request.accept, content_types))
+            raise ClientSideError(error_message, status_code=406)
+        return False
+    elif request.method in ['PUT', 'POST', 'PATCH']:
+        content_type = request.headers.get('Content-Type')
+        if content_type:
+            for ct in content_types:
+                if request.headers.get('Content-Type', '').startswith(ct):
+                    return True
+            error_message = ('Unacceptable Content-Type: %s not in %s'
+                             % (content_type, content_types))
+            raise ClientSideError(error_message, status_code=415)
+        else:
+            raise ClientSideError('missing Content-Type header')
+    elif request.method in ['DELETE']:
+        return True
+    return False

wsme/root.py
@@ -230,20 +230,34 @@ class WSRoot(object):
 
         try:
             msg = None
+            error_status = 500
             protocol = self._select_protocol(request)
+            if protocol is None:
+                if request.method in ['GET', 'HEAD']:
+                    error_status = 406
+                elif request.method in ['POST', 'PUT', 'PATCH']:
+                    error_status = 415
+        except ClientSideError as e:
+            error_status = e.code
+            msg = e.faultstring
+            protocol = None
         except Exception as e:
-            msg = ("Error while selecting protocol: %s" % str(e))
+            msg = ("Unexpected error while selecting protocol: %s" % str(e))
             log.exception(msg)
             protocol = None
+            error_status = 500
 
         if protocol is None:
             if msg is None:
                 msg = ("None of the following protocols can handle this "
                        "request : %s" % ','.join((
                            p.name for p in self.protocols)))
-            res.status = 500
+            res.status = error_status
             res.content_type = 'text/plain'
-            res.text = u(msg)
+            try:
+                res.text = u(msg)
+            except TypeError:
+                res.text = msg
             log.error(msg)
             return res
 

wsme/tests/test_api.py
@@ -200,7 +200,7 @@ Value should be one of:"))
         app = webtest.TestApp(r.wsgiapp())
 
         res = app.get('/', expect_errors=True)
-        assert res.status_int == 500
+        assert res.status_int == 406
         print(res.body)
         assert res.body.find(
             b("None of the following protocols can handle this request")) != -1

wsme/tests/test_root.py
@@ -38,4 +38,5 @@ class TestRoot(unittest.TestCase):
         res = root._handle_request(req)
         assert res.status_int == 500
         assert res.content_type == 'text/plain'
-        assert res.text == u('Error while selecting protocol: test'), req.text
+        assert (res.text ==
+                'Unexpected error while selecting protocol: test'), req.text

wsme/rest/protocol.py
@@ -2,7 +2,7 @@ import os.path
 import logging
 
 from wsme.utils import OrderedDict
-from wsme.protocol import CallContext, Protocol
+from wsme.protocol import CallContext, Protocol, media_type_accept
 
 import wsme.rest
 import wsme.rest.args
@@ -34,12 +34,7 @@ class RestProtocol(Protocol):
         for dataformat in self.dataformats:
             if request.path.endswith('.' + dataformat):
                 return True
-        if request.headers.get('Accept') in self.content_types:
-            return True
-        for ct in self.content_types:
-            if request.headers['Content-Type'].startswith(ct):
-                return True
-        return False
+        return media_type_accept(request, self.content_types)
 
     def iter_calls(self, request):
         context = CallContext(request)

wsme/tests/test_restjson.py
@@ -351,7 +351,7 @@ class TestRestJson(wsme.tests.protocol.RestOnlyProtocolTestCase):
 
     def test_GET(self):
         headers = {
-            'Content-Type': 'application/json',
+            'Accept': 'application/json',
         }
         res = self.app.get(
             '/crud?ref.id=1',
@@ -362,7 +362,58 @@ class TestRestJson(wsme.tests.protocol.RestOnlyProtocolTestCase):
         print(result)
         assert result['data']['id'] == 1
         assert result['data']['name'] == u("test")
-        assert result['message'] == "read"
+
+    def test_GET_complex_accept(self):
+        headers = {
+            'Accept': 'text/html,application/xml;q=0.9,*/*;q=0.8'
+        }
+        res = self.app.get(
+            '/crud?ref.id=1',
+            headers=headers,
+            expect_errors=False)
+        print("Received:", res.body)
+        result = json.loads(res.text)
+        print(result)
+        assert result['data']['id'] == 1
+        assert result['data']['name'] == u("test")
+
+    def test_GET_complex_choose_xml(self):
+        headers = {
+            'Accept': 'text/html,text/xml;q=0.9,*/*;q=0.8'
+        }
+        res = self.app.get(
+            '/crud?ref.id=1',
+            headers=headers,
+            expect_errors=False)
+        print("Received:", res.body)
+        assert res.content_type == 'text/xml'
+
+    def test_GET_complex_accept_no_match(self):
+        headers = {
+            'Accept': 'text/html,application/xml;q=0.9'
+        }
+        res = self.app.get(
+            '/crud?ref.id=1',
+            headers=headers,
+            status=406)
+        print("Received:", res.body)
+        assert res.body == ("Unacceptable Accept type: "
+                            "text/html, application/xml;q=0.9 not in "
+                            "['application/json', 'text/javascript', "
+                            "'application/javascript', 'text/xml']")
+
+    def test_GET_bad_simple_accept(self):
+        headers = {
+            'Accept': 'text/plain',
+        }
+        res = self.app.get(
+            '/crud?ref.id=1',
+            headers=headers,
+            status=406)
+        print("Received:", res.body)
+        assert res.body == ("Unacceptable Accept type: text/plain not in "
+                            "['application/json', 'text/javascript', "
+                            "'application/javascript', 'text/xml']")
 
     def test_POST(self):
         headers = {
@@ -380,6 +431,20 @@ class TestRestJson(wsme.tests.protocol.RestOnlyProtocolTestCase):
         assert result['data']['name'] == u("test")
         assert result['message'] == "update"
 
+    def test_POST_bad_content_type(self):
+        headers = {
+            'Content-Type': 'text/plain',
+        }
+        res = self.app.post(
+            '/crud',
+            json.dumps(dict(data=dict(id=1, name=u('test')))),
+            headers=headers,
+            status=415)
+        print("Received:", res.body)
+        assert res.body == ("Unacceptable Content-Type: text/plain not in "
+                            "['application/json', 'text/javascript', "
+                            "'application/javascript', 'text/xml']")
+
     def test_DELETE(self):
         res = self.app.delete(
             '/crud.json?ref.id=1',
@@ -393,7 +458,7 @@ class TestRestJson(wsme.tests.protocol.RestOnlyProtocolTestCase):
 
     def test_extra_arguments(self):
         headers = {
-            'Content-Type': 'application/json',
+            'Accept': 'application/json',
         }
         res = self.app.get(
             '/crud?ref.id=1&extraarg=foo',

wsmeext/tests/test_sqlalchemy_controllers.py
@@ -136,11 +136,25 @@ class TestCRUDController():
         DBSession.flush()
         pid = p.id
         r = self.app.get('/person?ref.id=%s' % pid,
-                         headers={'Content-Type': 'application/json'})
+                         headers={'Accept': 'application/json'})
         r = json.loads(r.text)
         print(r)
         assert r['name'] == u('Pierre-Joseph')
         assert r['birthdate'] == u('1809-01-15')
+
+    def test_GET_bad_accept(self):
+        p = DBPerson(
+            name=u('Pierre-Joseph'),
+            birthdate=datetime.date(1809, 1, 15))
+        DBSession.add(p)
+        DBSession.flush()
+        pid = p.id
+        r = self.app.get('/person?ref.id=%s' % pid,
+                         headers={'Accept': 'text/plain'},
+                         status=406)
+        assert r.text == ("Unacceptable Accept type: text/plain not in "
+                          "['application/json', 'text/javascript', "
+                          "'application/javascript', 'text/xml']")
 
     def test_update(self):
         p = DBPerson(

commit e31045e57a102182d33252e3a6b07ddfa9488ebe

Multiple protocol accept or content-type matching
The changes in 8710dabb652dae775dee31789e91608f832e62e6 broke
protocol failover when the REST protocol is listed before others
(see bug referenced below).

This patch tries to solve both issues by trying to match accept over all
the protocols, only giving up a 406 or 415 if all protocols fail, using
the last failure as the error message.

Related-Bug: #1419110
Closes-Bug: #1442710
Change-Id: I328a392151013c46207519c245213d5dec48ecc9

wsme/protocol.py
@@ -117,7 +117,7 @@ def getprotocol(name, **options):
 
 
 def media_type_accept(request, content_types):
-    """Return True if the requested media type is available.
+    """Validate media types against request.method.
 
     When request.method is GET or HEAD compare with the Accept header.
     When request.method is POST, PUT or PATCH compare with the Content-Type
@@ -131,7 +131,6 @@ def media_type_accept(request, content_types):
             error_message = ('Unacceptable Accept type: %s not in %s'
                              % (request.accept, content_types))
             raise ClientSideError(error_message, status_code=406)
-        return False
     elif request.method in ['PUT', 'POST', 'PATCH']:
         content_type = request.headers.get('Content-Type')
         if content_type:

wsme/root.py
@@ -149,6 +149,7 @@ class WSRoot(object):
                       request.body[:512] or
                       request.body) or '')
         protocol = None
+        error = ClientSideError(status_code=406)
         path = str(request.path)
         assert path.startswith(self._webpath)
         path = path[len(self._webpath) + 1:]
@@ -157,9 +158,16 @@ class WSRoot(object):
         else:
 
             for p in self.protocols:
-                if p.accept(request):
-                    protocol = p
-                    break
+                try:
+                    if p.accept(request):
+                        protocol = p
+                        break
+                except ClientSideError as e:
+                    error = e
+            # If we could not select a protocol, we raise the last exception
+            # that we got, or the default one.
+            if not protocol:
+                raise error
         return protocol
 
     def _do_call(self, protocol, context):
@@ -232,11 +240,6 @@ class WSRoot(object):
             msg = None
             error_status = 500
             protocol = self._select_protocol(request)
-            if protocol is None:
-                if request.method in ['GET', 'HEAD']:
-                    error_status = 406
-                elif request.method in ['POST', 'PUT', 'PATCH']:
-                    error_status = 415
         except ClientSideError as e:
             error_status = e.code
             msg = e.faultstring
@@ -248,7 +251,7 @@ class WSRoot(object):
             error_status = 500
 
         if protocol is None:
-            if msg is None:
+            if not msg:
                 msg = ("None of the following protocols can handle this "
                        "request : %s" % ','.join((
                            p.name for p in self.protocols)))
@@ -296,6 +299,10 @@ class WSRoot(object):
             else:
                 res.status = protocol.get_response_status(request)
                 res_content_type = protocol.get_response_contenttype(request)
+        except ClientSideError as e:
+            request.server_errorcount += 1
+            res.status = e.code
+            res.text = e.faultstring
         except Exception:
             infos = wsme.api.format_exception(sys.exc_info(), self._debug)
             request.server_errorcount += 1

wsme/tests/test_api.py
@@ -201,6 +201,7 @@ Value should be one of:"))
         app = webtest.TestApp(r.wsgiapp())
 
         res = app.get('/', expect_errors=True)
+        print(res.status_int)
         assert res.status_int == 406
         print(res.body)
         assert res.body.find(

wsme/tests/test_root.py
@@ -3,9 +3,12 @@
 import unittest
 
 from wsme import WSRoot
+import wsme.protocol
+import wsme.rest.protocol
 from wsme.root import default_prepare_response_body
 
 from six import b, u
+from webob import Request
 
 
 class TestRoot(unittest.TestCase):
@@ -24,9 +27,9 @@ class TestRoot(unittest.TestCase):
         default_prepare_response_body(None, [u('a'), u('b')]) == u('a\nb')
 
     def test_protocol_selection_error(self):
-        import wsme.protocol
-
         class P(wsme.protocol.Protocol):
+            name = "test"
+
             def accept(self, r):
                 raise Exception('test')
 
@@ -40,3 +43,80 @@ class TestRoot(unittest.TestCase):
         assert res.content_type == 'text/plain'
         assert (res.text ==
                 'Unexpected error while selecting protocol: test'), req.text
+
+    def test_protocol_selection_accept_mismatch(self):
+        """Verify that we get a 406 error on wrong Accept header."""
+        class P(wsme.protocol.Protocol):
+            name = "test"
+
+            def accept(self, r):
+                return False
+
+        root = WSRoot()
+        root.addprotocol(wsme.rest.protocol.RestProtocol())
+        root.addprotocol(P())
+
+        req = Request.blank('/test?check=a&check=b&name=Bob')
+        req.method = 'GET'
+        res = root._handle_request(req)
+        assert res.status_int == 406
+        assert res.content_type == 'text/plain'
+        assert res.text.startswith(
+            'None of the following protocols can handle this request'
+        ), req.text
+
+    def test_protocol_selection_content_type_mismatch(self):
+        """Verify that we get a 415 error on wrong Content-Type header."""
+        class P(wsme.protocol.Protocol):
+            name = "test"
+
+            def accept(self, r):
+                return False
+
+        root = WSRoot()
+        root.addprotocol(wsme.rest.protocol.RestProtocol())
+        root.addprotocol(P())
+
+        req = Request.blank('/test?check=a&check=b&name=Bob')
+        req.method = 'POST'
+        req.headers['Content-Type'] = "test/unsupported"
+        res = root._handle_request(req)
+        assert res.status_int == 415
+        assert res.content_type == 'text/plain'
+        assert res.text.startswith(
+            'Unacceptable Content-Type: test/unsupported not in'
+        ), req.text
+
+    def test_protocol_selection_get_method(self):
+        class P(wsme.protocol.Protocol):
+            name = "test"
+
+            def accept(self, r):
+                return True
+
+        root = WSRoot()
+        root.addprotocol(wsme.rest.protocol.RestProtocol())
+        root.addprotocol(P())
+
+        req = Request.blank('/test?check=a&check=b&name=Bob')
+        req.method = 'GET'
+        req.headers['Accept'] = 'test/fake'
+        p = root._select_protocol(req)
+        assert p.name == "test"
+
+    def test_protocol_selection_post_method(self):
+        class P(wsme.protocol.Protocol):
+            name = "test"
+
+            def accept(self, r):
+                return True
+
+        root = WSRoot()
+        root.addprotocol(wsme.rest.protocol.RestProtocol())
+        root.addprotocol(P())
+
+        req = Request.blank('/test?check=a&check=b&name=Bob')
+        req.headers['Content-Type'] = 'test/fake'
+        req.method = 'POST'
+        p = root._select_protocol(req)
+        assert p.name == "test"