002473c0eaff5bde539e6f6980c387d3817f7063
Lan Qi song <lqslan@cn.ibm.com>
2015-05-15 17:32:39 +0800

    Move ipaddr to netaddr
    Ipaddr module was never introduced by openstack and never used by
    other openstack components. We should move to more reliable module
    netaddr to validate ip format.
    
    Change-Id: I38d0c6f4ea2147ce071ab62a7c9d546436aec185
    Closes-Bug: #1455404

--- wsme/tests/test_types.py
+++ wsme/tests/test_types.py
@@ -339,20 +339,24 @@ Value: 'v3'. Value should be one of: v., v.",
         v = types.IPv4AddressType()
         self.assertEqual(v.validate('127.0.0.1'), '127.0.0.1')
         self.assertEqual(v.validate('192.168.0.1'), '192.168.0.1')
+        self.assertEqual(v.validate(u'8.8.1.1'), u'8.8.1.1')
         self.assertRaises(ValueError, v.validate, '')
         self.assertRaises(ValueError, v.validate, 'foo')
         self.assertRaises(ValueError, v.validate,
                           '2001:0db8:bd05:01d2:288a:1fc0:0001:10ee')
+        self.assertRaises(ValueError, v.validate, '1.2.3')
 
     def test_validate_ipv6_address_type(self):
         v = types.IPv6AddressType()
         self.assertEqual(v.validate('0:0:0:0:0:0:0:1'),
                          '0:0:0:0:0:0:0:1')
+        self.assertEqual(v.validate(u'0:0:0:0:0:0:0:1'), u'0:0:0:0:0:0:0:1')
         self.assertEqual(v.validate('2001:0db8:bd05:01d2:288a:1fc0:0001:10ee'),
                          '2001:0db8:bd05:01d2:288a:1fc0:0001:10ee')
         self.assertRaises(ValueError, v.validate, '')
         self.assertRaises(ValueError, v.validate, 'foo')
         self.assertRaises(ValueError, v.validate, '192.168.0.1')
+        self.assertRaises(ValueError, v.validate, '0:0:0:0:0:0:1')
 
     def test_validate_uuid_type(self):
         v = types.UuidType()
