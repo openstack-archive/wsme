2cb266eadb8a0147328433ea85370cb68cf30e6f
Mehdi Abaakouk <sileht@sileht.net>
2015-08-03 14:30:31 +0200

    Fixes exception path with the datatype is a Object
    If the object type is ArrayType or DictType, the datatype
    is an object not a class. Currently a 500 error is raise
    just because the exception path for invalid input handle only
    when the datatype is an UserType or Class, not object.
    
    This change fixes that.
    
    Change-Id: Ifadef698a4dca0d33167bd4d5a567c43fe015108
    Closes-bug: #1428185

--- wsme/tests/test_protocols_commons.py
+++ wsme/tests/test_protocols_commons.py
@@ -7,7 +7,11 @@ from wsme.api import FunctionArgument, FunctionDefinition
 from wsme.rest.args import from_param, from_params, args_from_args
 from wsme.exc import InvalidInput
 
-from wsme.types import UserType, Unset, ArrayType, DictType
+from wsme.types import UserType, Unset, ArrayType, DictType, Base
+
+
+class MyBaseType(Base):
+    test = str
 
 
 class MyUserType(UserType):
@@ -86,6 +90,17 @@ class TestProtocolsCommons(unittest.TestCase):
             args_from_args(fd, ['invalid-argument'], {})
         except InvalidInput as e:
             assert fake_type.name in str(e)
+        else:
+            self.fail('Should have thrown an InvalidInput')
+
+    def test_args_from_args_array_type(self):
+        fake_type = ArrayType(MyBaseType)
+        fd = FunctionDefinition(FunctionDefinition)
+        fd.arguments.append(FunctionArgument('fake-arg', fake_type, True, []))
+        try:
+            args_from_args(fd, [['invalid-argument']], {})
+        except InvalidInput as e:
+            assert ArrayType.__name__ in str(e)
         else:
             self.fail('Should have thrown an InvalidInput')
 
